ed91ee6b336c266c73c841d19bc8c8f0b2d5e5d0
Joshua Pettingill <josh.d.pettingill@gmail.com>
2022-05-13 10:37:18 -0600

    Added more functionality to compressor

--- compression.py
+++ compression.py
@@ -1,10 +1,14 @@
-
+import os.path
 
 class compressor(object):
-    def __init__(self):
-        pass
+    def __init__(self, row_length=5, col_height=5) -> None:
+        self.row_length = row_length
+        self.col_height = col_height
 
     def compress(self, filepath):
+        if not os.path.exists(filepath):
+            raise(FileNotFoundError())
+        # self.create_grid(filepath)
         newFilepath = filepath.replace("txt", "lor")
-        with open(newFilepath, 'w') as f:
+        with open(newFilepath, 'a') as f:
             f.write("")
--- test_compression.py
+++ test_compression.py
@@ -9,8 +9,22 @@ class TestCompressor(unittest.TestCase):
     def setUp(self):
         self.compressor = compression.compressor()
 
-    def test_compressor_creates_compressed_file(self):
-        self.compressor.compress("".join([TEST_FILE_FOLDER,"\\random_bit_string_1.txt"]))
-        self.assertTrue(os.path.exists("".join([TEST_FILE_FOLDER, "\\random_bit_string_1.lor"])))
+    def test_compressor_grid_width_is_an_integer(self):
+        self.assertIsInstance(self.compressor.row_length, int)
 
-    # def test_
+    def test_compressor_grid_height_is_an_integer(self):
+        self.assertIsInstance(self.compressor.col_height, int)
+
+    def test_compress_when_given_nothing(self):
+        self.assertRaises(FileNotFoundError, self.compressor.compress, "")
+
+    def test_compressor_creates_compressed_file(self):
+        self.compressor.compress("".join([TEST_FILE_FOLDER,"\\random_bit_strings_1.txt"]))
+        self.assertTrue(os.path.exists("".join([TEST_FILE_FOLDER, "\\random_bit_strings_1.lor"])))
+
+    # def test_can_create_grid()
+
+    # def test_compress_creates_grid(self):
+    #     self.compressor.compress("")
+    #     self.assertEqual(self.compressor.grid, [])
+
