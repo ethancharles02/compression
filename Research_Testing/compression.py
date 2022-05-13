243dbdb02a144cdec5d48dcb405178711f5cbdb4
Joshua Pettingill <josh.d.pettingill@gmail.com>
2022-05-13 10:54:35 -0600

    Preparing to merge with other branch for testing
    Co-authored-by: Ethan Charles <ethancharles02@users.noreply.github.com>

--- Research_Testing/compression.py
+++ Research_Testing/compression.py
@@ -1,13 +1,20 @@
 import os.path
 
 class compressor(object):
-    def __init__(self, row_length=5, col_height=5) -> None:
+    def __init__(self, row_length=16, col_height=64) -> None:
         self.row_length = row_length
         self.col_height = col_height
+        self.chunk_size = self.col_height * self.row_length
 
     def compress(self, filepath):
         if not os.path.exists(filepath):
             raise(FileNotFoundError())
+
+        with open(filepath, 'r') as f:
+            data = f.read(self.chunk_size)
+
+        # with open(filepath, 'rb') as f:
+        #     data = f.read(self.chunck_size)
         # self.create_grid(filepath)
         newFilepath = filepath.replace("txt", "lor")
         with open(newFilepath, 'a') as f:
--- Research_Testing/test_compression.py
+++ Research_Testing/test_compression.py
@@ -15,6 +15,9 @@ class TestCompressor(unittest.TestCase):
     def test_compressor_grid_height_is_an_integer(self):
         self.assertIsInstance(self.compressor.col_height, int)
 
+    def test_compressor_chunck_size_is_an_integer(self):
+        self.assertIsInstance(self.compressor.chunk_size, int)
+
     def test_compress_when_given_nothing(self):
         self.assertRaises(FileNotFoundError, self.compressor.compress, "")
 
@@ -22,6 +25,9 @@ class TestCompressor(unittest.TestCase):
         self.compressor.compress("".join([TEST_FILE_FOLDER,"\\random_bit_strings_1.txt"]))
         self.assertTrue(os.path.exists("".join([TEST_FILE_FOLDER, "\\random_bit_strings_1.lor"])))
 
+    def test_reads_correct_chunk_size(self):
+        self.compressor.compress("".join([TEST_FILE_FOLDER,"\\random_bit_strings_1.txt"]))
+        
     # def test_can_create_grid()
 
     # def test_compress_creates_grid(self):
