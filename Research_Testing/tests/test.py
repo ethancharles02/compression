2778c7890970adcbd87bbbeac0f40879e185947a
Joshua Pettingill <josh.d.pettingill@gmail.com>
2022-06-25 21:54:43 -0600

    fixed error with file used for debugging

--- Research_Testing/tests/test.py
+++ Research_Testing/tests/test.py
@@ -3,14 +3,14 @@ from sys import path
 path.append("..")
 from text_compression_algorithm import Text_Compression_Algorithm
 
-compressor = Text_Compression_Algorithm(5)
-compressor.compress("\nword")
+# compressor = Text_Compression_Algorithm(5)
+# compressor.compress("\nword")
 # string1 = "word n1 n2 n3 n4 n5\n"
 # expected_string = "word n1 n2 n3 n4 n5\nword n1 n2 n3 n4 n5\nword n1 n2 n3 n4 n5\nword n1 n2 n3 n4 n5\nword n1 n2 n3 n4 n5\nword n1 n2 n3 n4 n5\nword n1 n2 n3 n4 n5\nword n1 n2 n3 n4 n5\nword n1 n2 n3 n4 n5\nword n1 n2 n3 n4 n5\nword n1 n2 n3 n4 n5\n"
 # for i in range(11):
 #     compressor.compress(string1)   
-print(f"Result:\n{compressor.get_compressed_data()}")
-print(f"Expected:\n{expected_string}")
+# print(f"Result:\n{compressor.get_compressed_data()}")
+# print(f"Expected:\n{expected_string}")
 
 # Text Compressor
 # from sys import path
